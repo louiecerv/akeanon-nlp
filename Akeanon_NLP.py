6e7b227a95fda6745d4903e4d564786e8d185bf3
louiecerv <lfcervantes@wvsu.edu.ph>
2024-04-14 19:33:56 +0800

    Save

--- Akeanon_NLP.py
+++ Akeanon_NLP.py
@@ -3,7 +3,6 @@ import google.generativeai as genai
 import streamlit as st
 import os
 import time
-from google.generativeai import Model
 
 
 #GOOGLE_API_KEY=st.secrets["GOOGLE_API_KEY"]
@@ -22,19 +21,7 @@ def app():
 
     st.write(base_model)
      
-    Model(name='models/gemini-1.0-pro-001',
-        base_model_id='',
-        version='001',
-        display_name='Gemini 1.0 Pro 001 (Tuning)',
-        description=('The best model for scaling across a wide range of tasks. This is a stable '
-                    'model that supports tuning.'),
-        input_token_limit=30720,
-        output_token_limit=2048,
-        supported_generation_methods=['generateContent', 'countTokens', 'createTunedModel'],
-        temperature=0.9,
-        top_p=1.0,
-        top_k=1)  
-   
+
     st.write("Powered by Google Cloud Natural Language API")
 
 #run the app
